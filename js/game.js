a4a302911d852525705532def2d8aac40964c2a9
Hannes S <info@hannes-stein.com>
2015-05-05 16:42:21 +0200

    Added the game class

--- js/game.js
+++ js/game.js
@@ -1,4 +1,32 @@
 (function() {
     "use strict";
 
+    var gameInstance;
+
+    /**
+     * Game
+     */
+    var Game = (function() {
+
+        var Class = function() {
+
+            this._settings = {
+                                "frameAmount": 10,
+                                "rollsPerFrame": 2,
+                                "pinAmount": 10
+                             };
+
+        };
+
+        Class.prototype = {
+
+        };
+
+        return Class;
+
+    })();
+
+    gameInstance = new Game();
+    console.log(gameInstance._settings.frameAmount);
+
 })();
