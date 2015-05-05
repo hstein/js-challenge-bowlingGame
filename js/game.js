c3b95d4e54d8ebed49ab1054aae83ec0800203f1
Hannes S <info@hannes-stein.com>
2015-05-05 16:52:52 +0200

    Added the player class

--- js/game.js
+++ js/game.js
@@ -15,6 +15,34 @@
                                 "rollsPerFrame": 2,
                                 "pinAmount": 10
                              };
+            this._players = [];
+
+        };
+
+        Class.prototype = {
+
+            init: function init() {
+
+            },
+
+            addPlayer: function addPlayer(player) {
+                this._players.push(player);
+            }
+
+        };
+
+        return Class;
+
+    })();
+
+    /**
+     * Player
+     */
+    var Player = (function(name) {
+
+        var Class = function(name) {
+
+            this.name = name || 'John Doe';
 
         };
 
@@ -26,7 +54,12 @@
 
     })();
 
+    /**
+     * Game creation
+     */
     gameInstance = new Game();
-    console.log(gameInstance._settings.frameAmount);
+    gameInstance.addPlayer(new Player('Peter Pan'));
+    gameInstance.addPlayer(new Player());
+    console.log(gameInstance._players);
 
 })();
